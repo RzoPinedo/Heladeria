bc38db0b9d349d2301677d9b84e2210e404d96ae
Leo Pinedo <86122548+RzoPinedo@users.noreply.github.com>
2021-12-11 01:15:14 -0500

    tienda con pasarela de pago

---
+++ js/envio.js
@@ -0,0 +1,19 @@
+
+function getData (){
+    var nombre = document.getElementById("inputNombre").value;
+    var correo = document.getElementById("inputCorreo").value;
+    var mensaje = document.getElementById("inputMensaje").value;    
+
+    alert("Nombres y Apellidos:  " + nombre + "\n"
+    + "Correo:  " + correo + "\n"
+    + "Mensaje:  " + mensaje);
+}
+
+// var getData = function (){
+//     var name = document.getElementById("inputName").values
+//     var correo = document.getElementById("inputCorreo").values;
+//     var mensaje = document.getElementById("inputMensaje").values;
+
+//     document.write(name);
+
+// }
